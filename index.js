4c715ba64451d6430ac492328846ec324b677d95
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-03-24 13:24:58 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ['Milo','Otis','Garfield']
+
+function destructivelyAppendCat(Ralph){
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function appendCat(name){
+    return[...cats, 'Broom'];
+}
+
+function prependCat(name){
+    return['Arnold', ...cats];
+}
+
+function removeLastCat(name){
+    return cats.slice(0, cats.length -1);
+}
+removeLastCat();
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
+removeFirstCat();
+
